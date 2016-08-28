6e448289d0d5a2323d963b9d026146a35a68d46e
alishalam <alisha.alam2012@gmail.com>
2016-08-28 14:32:13 -0700

    Initial Commit

---
+++ app/src/main/java/com/codepath/android/themeswitcher/app/ThemeApplication.java
@@ -0,0 +1,10 @@
+package com.codepath.android.themeswitcher.app;
+
+import android.app.Application;
+
+/**
+ * Created by alishaalam on 8/28/16.
+ */
+public class ThemeApplication extends Application {
+    public static int currentPosition;
+}
